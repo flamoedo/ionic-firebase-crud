bd30a3fa89970df196317c06935bd3f39c1ed1d7
Filho <flavio.filho@capgemini.com>
2018-09-25 16:42:44 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAyDROFpRrrjXnTTK40IojzLhEmbAgEjBI",
+  authDomain: "bulletinboard-c8748.firebaseapp.com",
+  databaseURL: "https://bulletinboard-c8748.firebaseio.com",
+  projectId: "bulletinboard-c8748",
+  storageBucket: "bulletinboard-c8748.appspot.com",
+  messagingSenderId: "673198240037"
+};
 
 @Component({
   selector: 'app-root',
@@ -22,5 +32,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -3,9 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
 
+export const snapshotToArray = snapshot => {
+    let returnArr = [];
+
+    snapshot.forEach(childSnapshot => {
+        let item = childSnapshot.val();
+        item.key = childSnapshot.key;
+        returnArr.push(item);
+    });
+
+    return returnArr;
+};
+
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
